33708b3077c27b80350266b958455edb5f5df588
evicacoelho <evicacoelho@gmail.com>
2025-01-10 09:33:36 -0300

    :construction: Fixing server index

--- servers/index.js
+++ servers/index.js
@@ -7,6 +7,7 @@ require('dotenv').config(); // Load .env file
 const PORT = process.env.PORT || 3001;
 
 const app = express();
+app.use(express.static(path.resolve(__dirname, '../build')))
 app.use(cors());
 app.use(bodyParser.json());
 
@@ -55,6 +56,10 @@ app.post("/api/contact", (req, res) => {
         }
     });
 });
+
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../build', 'index.html'))
+})
 
 app.listen(PORT, () => {
     console.log(`[INFO] Server is online on port: ${PORT}`);
